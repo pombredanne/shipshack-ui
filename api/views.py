2fc7b4554cde75664acf0e47de08e08511837102
Can Mustafa Ozdemir <canmustafaozdemir@gmail.com>
2016-09-14 20:55:26 +0300

    get all entities

--- api/views.py
+++ api/views.py
@@ -17,19 +17,23 @@ class Photos(ViewSet):
     def list(self, request, path=''):
         client = GStorageClient()
         results = client.list_directory(settings.STORAGE_BUCKET, path)
-        results = [
-            self._process_item(i, settings.STORAGE_BUCKET) for i in results]
+        results = [self._process_item(i) for i in results]
         return Response(results)
 
-    def _get_file_props(self, item, bucket_name):
-        url = self.signer.get_url('/%s/%s' % (bucket_name, item.name))
+    def _get_signed_url(self, bucket, item):
+        return self.signer.get_url('/%s/%s' % (bucket, item.name))
+
+    def _get_file_props(self, item):
+        url = self._get_signed_url(settings.STORAGE_BUCKET, item)
+        thumb = self._get_signed_url(settings.THUMBNAIL_BUCKET, item)
+        source = self._get_signed_url(settings.SOURCE_BUCKET, item)
         return {'name': item.name, 'is_directory': False,
-                'url': url}
+                'url': url, 'thumbnail': thumb, 'source': source}
 
     def _get_directory_props(self, item):
         return {'name': item.name[:-1], 'is_directory': True}
 
-    def _process_item(self, item, bucket_name):
+    def _process_item(self, item):
         if isinstance(item, boto.gs.key.Key):
-            return self._get_file_props(item, bucket_name)
+            return self._get_file_props(item)
         return self._get_directory_props(item)
