8109d75ee6f59be10958af2af1122976d46fcc6b
Can Mustafa Ozdemir <canmustafaozdemir@gmail.com>
2016-09-15 17:43:22 +0300

    do not add irrelevant stuff to static files

--- shipshack/settings.py
+++ shipshack/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 SITE_URL = 'http://localhost:8000/'
